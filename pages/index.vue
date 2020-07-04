d3470279890aae3488aaa249e6ccffea1dedf002
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-04 12:31:38 +0700

    :wrench: Set predefined input

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      input: "",
+      input: "คุณสามารถอธิบายเรื่องยากด้วยคำที่ง่ายลงได้หรือไม่",
       output: "",
     }
   },
